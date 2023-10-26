de3e79837b50ecb926580d92647afd0474c3ef9c
umeshdoriyapatidar <96281455+umeshdoriyapatidar@users.noreply.github.com>
2023-10-26 12:49:33 +0000

    comparison

---
+++ _basic/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log(2>3);
+// console.log(2<3);
+// console.log(2>=3);
+// console.log(2!=3);
+// console.log(2==3);
+
+// console.log("0" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined >= 0);
+
+// note- the above comparison are restricated advise not to use or avoid this type of comaparison in js
+
+//  stric check
+ console.log( "2" === 2);
