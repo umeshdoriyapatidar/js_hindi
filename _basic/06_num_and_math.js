26fa6f1b5eb4b8af12dd6abe1aa50b4730f6845a
umeshdoriyapatidar <96281455+umeshdoriyapatidar@users.noreply.github.com>
2023-10-30 12:00:56 +0000

    maths and num

---
+++ _basic/06_num_and_math.js
@@ -0,0 +1,38 @@
+const  score = 300
+
+const balance = new Number(100)
+
+// console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+const other_number = 8999.899
+// console.log(other_number.toPrecision(3))
+
+
+const hundereds = 1000000
+// console.log(hundereds.toLocaleString("en-IN"));
+
+
+// ********************************************** MATHS ***********************************************************
+
+
+// console.log(Math);
+
+// console.log(Math.abs(-2))
+
+// console.log(Math.sin(30));
+// console.log(Math.round(4.5))
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+
+console.log((Math.floor((Math.random() * 10) +  1)));
+
+const min = 1;
+const max = 6;
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min ));
